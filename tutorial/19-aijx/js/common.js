3575267a579cc0b5d16ab3ce55b459170fe3acc4
ininindas <ehdrh248@naver.com>
2024-06-11 12:42:58 +0900

    init

--- tutorial/19-aijx/js/common.js
+++ tutorial/19-aijx/js/common.js
@@ -1,5 +1,6 @@
 const modleList = document.querySelector('.model_list')
 const modleData = './db/modle.json'
+const userData = './db/user.json'
 
 
 async function gteModles() {
@@ -15,9 +16,27 @@ async function gteModles() {
                             </div>
                             <div class="title">여자 1</div>
                         </li>`
-                        modleList.append(li)
+         modleList.append(li)
     })
 
 }
 
 gteModles()
+
+
+async function getUsers (){
+    const res = await fetch(userData)
+    const items = await res.json()
+
+    items.foreach(item => {
+        const tr = document.createElement('tr')
+        tr.innerHTML = `<td>${item.id}</td>
+						<td>${item.frist_name} ${item.last_name}</td>
+						<td>${item.email}</td>
+						<td>${item.gender}</td>
+						<td>${item.ip_address}</td>`
+         modleList.append(li)
+    })
+}
+
+getUsers()
