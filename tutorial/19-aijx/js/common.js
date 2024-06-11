121f1cd89fc30b0522c62fd755d917a9f1bcb5a8
ininindas <ehdrh248@naver.com>
2024-06-11 14:10:17 +0900

    init

--- tutorial/19-aijx/js/common.js
+++ tutorial/19-aijx/js/common.js
@@ -1,41 +1,41 @@
-const modleList = document.querySelector('.model_list')
-const modleData = './db/modle.json'
+const modelList = document.querySelector('.model_list')
+const modelData = './db/model.json'
 const userData = './db/user.json'
 
-
-async function gteModles() {
-    const res = await fetch(modleData)
+async function getModels() {
+    const res = await fetch(modelData)
     const data = await res.json()
-    const items = data.modle
+    const items = data.model
 
-    items.foreach(item => {
+    items.forEach(item => {       
         const li = document.createElement('li')
         li.innerHTML = `<li>
                             <div class="photo">
-                                <img src="../images/photo_01.png" alt="">
+                            <img src=${item.image} alt="">
                             </div>
-                            <div class="title">여자 1</div>
+                            <div class="title">${item.title}</div>
                         </li>`
-         modleList.append(li)
+        modelList.append(li)                
     })
-
 }
 
-gteModles()
+getModels()
 
+
+const tBody = document.querySelector('.tbl_body')
 
 async function getUsers (){
     const res = await fetch(userData)
     const items = await res.json()
 
-    items.foreach(item => {
+    items.forEach(item => {
         const tr = document.createElement('tr')
         tr.innerHTML = `<td>${item.id}</td>
 						<td>${item.frist_name} ${item.last_name}</td>
 						<td>${item.email}</td>
 						<td>${item.gender}</td>
 						<td>${item.ip_address}</td>`
-         modleList.append(li)
+        tBody.append(tr)
     })
 }
 
